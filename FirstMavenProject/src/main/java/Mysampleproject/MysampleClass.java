2ac284daa59190fbc021ae6c179bcf5e560b50d1
msman <msman@192.168.1.4>
2024-12-02 21:27:16 +0530

    this is a sample commit

---
+++ FirstMavenProject/src/main/java/Mysampleproject/MysampleClass.java
@@ -0,0 +1,10 @@
+package Mysampleproject;
+
+public class MysampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
